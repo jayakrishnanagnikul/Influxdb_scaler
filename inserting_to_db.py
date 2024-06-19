596a6e1f1ce22647e5d667e2e1b389ecb52f88bb
jayakrishnan <jayakrishnankannan@agnikul.in>
2024-06-19 13:05:08 +0530

    to commit 	 created 10 sensors 2khz data for 2 seconds
    observations
    
    data generation started 	1718781909.369347
    data generation completed	1718781932.1575027
    data writing completed diff is  0.5608389377593994
    start_write 1718781932.1575475 end_write 1718781932.7183864
    
    data is 4000 points x  10 sensors each

--- inserting_to_db.py
+++ inserting_to_db.py
@@ -1,11 +1,10 @@
 import numpy as np
 import time
-import matplotlib.pyplot as plt
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-
 # Parameters
+
 bucket = "test_bucket"
 org = "Agnikul"
 token = "sOUQsISLh_NrZMiDLYOrd9tnhgD-GRppfuQus1WQfbsW_FOMVUIm_c-2o428MOySuUyLZixLkSwl6jWUA_9b_Q=="
@@ -21,36 +20,41 @@ phase = 0          # Phase in radians
 sampling_rate = 1000  # Sampling rate in samples per second
 duration = 2       # Duration in seconds
 
-sine_wave_data=[]
 
 start_time = time.time_ns()
-
-for i in range(0,2000):
-
-    current_time = start_time + i * int(1.0 / sampling_rate * 1e9)  # 0.1 seconds in nanoseconds
-    sine_value = amplitude * np.sin(2 * np.pi * frequency * current_time * 1e-9 + phase)
-    sine_value2 = amplitude * np.sin(2 * np.pi * frequency * current_time * 1e-9 + 10)
-    sine_value3 = amplitude * np.sin(2 * np.pi * frequency * current_time * 1e-9 + 3)
-
-
+temp=[]
+point_list=[]
+data_start_time=time.time()
+print(data_start_time)
+#iterates over all sensors
+for sen_no in range(1,10):
+    point=[]
+    time_list=[]
+    sine_wave_data=[]
     
-    point = (
-        Point("sine_wave")
-        .tag("device", "sensor")
-        .field("value", sine_value)
-        .time(current_time , WritePrecision.NS))
-    
-    point2=(Point("sine_wave")
-        .tag("device", "sensor2")
-        .field("value", sine_value2)
-        .time(current_time , WritePrecision.NS))
-    
-    point3=(Point("sine_wave")
-        .tag("device", "sensor3")
-        .field("value", sine_value3)
-        .time(current_time , WritePrecision.NS))
+    #creates whole sensor values 
+    for i in range(0,2000):
+        time_data = start_time + i * int(1.0 / sampling_rate * 1e9)  # 0.1 seconds in nanoseconds
+        input_data = amplitude *sen_no/100* np.sin(2 * np.pi * frequency * time_data * 1e-9 + phase)
+        
+        sine_wave_data.append(input_data)
+        time_list.append(time_data)
+  
+        sensor_point= (
+            Point("sine_wave")
+            .tag("device", "sensor"f'{sen_no}')
+            .field("value", sine_wave_data[i])
+            .time(time_list[i] , WritePrecision.NS))
+        
+        point.append(sensor_point)
+        time.sleep(1.0 / sampling_rate)
+    point_list.append(point)
+    print("generated data for sensor ",sen_no,end="\r")
 
-    write_api.write(bucket=bucket, org=org, record=[point,point2,point3])
-    print("entered data",i,end="\r")
-    # time.sleep(1.0 / sampling_rate)
-    # print("\r")
+print("data generation completed",time.time())
+
+start_write=time.time()
+write_api.write(bucket=bucket, org=org, record=point_list)
+end_write=time.time()
+print("data writing completed diff is ",end_write-start_write)
+print("start_write",start_write,"end_write",end_write)
